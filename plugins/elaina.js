548a76031b30c139a661b00e61eb3c2ffa9bbe30
Kyouka Tachibana <91820453+Yuri-Neko@users.noreply.github.com>
2022-03-29 17:29:09 +0700

    Update and rename elaina.js to -milf.js

--- plugins/elaina.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, global.API('lolhuman', '/api/random/elaina', {}, 'apikey'), 'istri gweh', wm, 'NEXT', '.elaina', m)
-}
-handler.help = ['elaina']
-handler.tags = ['anime']
-handler.command = /^(elaina)$/i
-
-module.exports = handler
